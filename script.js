fefb06556c01863dd7db7e1168db0436a77d7742
Ryan Zeroual <6221740>
2024-08-27 12:54:34 -0400

    Added the getChoice , Fixed the values , Added variables that equals the main ones

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+let humanScore = 0
+let computerScore = 0
+
+
 function getComputerChoice(){
-let x = ["Rock","Paper","Scissors"]
-console.log(x[Math.floor(Math.random()*x.length)])
+    const choices= ["Rock","Paper","Scissors"]
+    const randomize= choices[Math.floor(Math.random()*choices.length)]
+    console.log("The computer chose: " +randomize)
+    return randomize
 }
-getComputerChoice();
+
+
 
 function getHumanChoice(){
-    
-
-
-
+    let hChoice = prompt("Choose Between Rock, Paper and Scissors!")
+    console.log("You chose: " +hChoice)
+    return hChoice
 }
+
+
+
+function playRound(humanChoice,computerChoice){
+    if  (humanChoice == computerChoice) {
+        console.log("Draw!")
+    } 
+}
+
+const huCHoice= getHumanChoice();
+const coChoice = getComputerChoice();
+playRound(huCHoice , coChoice);
